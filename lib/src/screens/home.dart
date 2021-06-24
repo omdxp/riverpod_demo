09b5b3d32aa38a824a069155f571d3dc59a96d79
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-06-24 18:16:32 +0100

    Future provider.

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -6,15 +6,30 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Consumer(
-        builder: (context, watch, child) {
-          final greetingText = watch(greetingProvider);
-          final count = watch(incrementProvider);
-          return Text(
-            greetingText + ' ' + count.value.toString(),
-            style: TextStyle(fontSize: 50),
-          );
-        },
+      child: Column(
+        children: [
+          Consumer(
+            builder: (context, watch, child) {
+              final greetingText = watch(greetingProvider);
+              final count = watch(incrementProvider);
+              return Text(
+                greetingText + ' ' + count.value.toString(),
+                style: TextStyle(fontSize: 50),
+              );
+            },
+          ),
+          Consumer(builder: (context, watch, child) {
+            final responseAsyncValue =
+                watch(responseProvider('https://flutter.space'));
+            return responseAsyncValue.map(
+                data: (_) => Text(_.value),
+                loading: (_) => CircularProgressIndicator(),
+                error: (_) => Text(
+                      _.error.toString(),
+                      style: TextStyle(color: Colors.red),
+                    ));
+          }),
+        ],
       ),
     );
   }
--- lib/src/utils/providers.dart
+++ lib/src/utils/providers.dart
@@ -3,6 +3,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final greetingProvider = Provider((ref) => 'Hello Riverpod!');
 final incrementProvider = ChangeNotifierProvider((ref) => IncrementNotifier());
+final fakeHttpClientProvider = Provider((ref) => FakeHttpClient());
+
+final responseProvider =
+    FutureProvider.autoDispose.family<String, String>((ref, url) async {
+  final httpClient = ref.read(fakeHttpClientProvider);
+  return httpClient.get(url);
+});
 
 class IncrementNotifier extends ChangeNotifier {
   int _value = 0;
@@ -11,5 +18,12 @@ class IncrementNotifier extends ChangeNotifier {
   void increment() {
     _value++;
     notifyListeners();
+  }
+}
+
+class FakeHttpClient {
+  Future<String> get(String url) async {
+    await Future.delayed(const Duration(seconds: 1));
+    return 'Response from $url';
   }
 }
